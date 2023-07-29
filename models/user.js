f7bc131b8f129552bbd0dd45e45b14b6da9a1a8f
Ageev Linar <ageevlinar@yandex.ru>
2023-07-29 19:33:44 +0300

    ......3

--- models/user.js
+++ models/user.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcryptjs');
+
 const validator = require('validator');
+
+const UnauthorizedError = require('../utils/UnauthоrizedError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -42,6 +46,22 @@ const userSchema = new mongoose.Schema({
 }, {
   versionKey: false,
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new UnauthorizedError('Неверный логин или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new UnauthorizedError('Неверный логин или пароль'));
+          }
+          return user;
+        });
+    });
+};
 
 const User = mongoose.model('user', userSchema);
 
