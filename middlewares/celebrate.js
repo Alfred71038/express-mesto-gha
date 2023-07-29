b9167a7253683bb2e5f1cf764a914c9075e0576f
Ageev Linar <ageevlinar@yandex.ru>
2023-07-29 23:17:24 +0300

    ......4

--- middlewares/celebrate.js
+++ middlewares/celebrate.js
@@ -40,7 +40,7 @@ const celebrateLogin = celebrate({
 
 const celebrateCreateCard = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().regex(regex),
   }),
 });
