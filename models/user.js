ce9eb8e05711db01b3490a346266827944a36b6a
Ageev Linar <ageevlinar@yandex.ru>
2023-07-29 17:01:04 +0300

    ПР14 Пробная итерация

--- models/user.js
+++ models/user.js
@@ -43,22 +43,6 @@ const userSchema = new mongoose.Schema({
   versionKey: false,
 });
 
-userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email }).select('+password')
-    .then((user) => {
-      if (!user) {
-        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
-      }
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
-          }
-          return user;
-        });
-    });
-};
-
 const User = mongoose.model('user', userSchema);
 
 module.exports = User;
